43a6aa3d5e1adb3d4597e3d635bb6ba45b49ef60
Max <mxlang@live.com>
2024-04-01 10:20:45 -0400

    Remove node_modules from version control

---
+++ src/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1 className="note h1">Note Title</h1>
+      <p className="note p">Note content goes here</p>
+    </div>
+  )
+}
+
+export default Note;
